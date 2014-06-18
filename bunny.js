a0dc1ec1f576e54d439afe8e967119ecfc27bb00
sijie liu <sijliu@ebay.com>
2014-06-18 15:09:08 -0700

    nav animi

--- bunny.js
+++ bunny.js
@@ -30,30 +30,40 @@ $(document).ready(function(){
 		},
 		15)
 	);
-	
-	$(".navbar-nav li.active").hover(
-		function(e){
-			$("#eye-right-bow").css({opacity: 1});
-			$("#eye-left-bow").css({opacity: 1, bottom: "200px"});
-		},
-		function(e){
-			$("#eye-left-bow").css({opacity: 0, bottom: "180px"});
-			$("#eye-right-bow").css({opacity: 0});
-		}
-	);
 
-	$(".navbar-nav li:not(.active)").hover(
+	$(".navbar-nav li").hover(
 		function(e){
- 			$("#eye-left-shine").css({opacity: 1, "transform": "scale(0.8,0.8)"});
-			$("#eye-right-shine").css({opacity: 1, "transform": "scale(0.8,0.8)"});//	height: 45px; width: 46px;
-			$("#eye-right-bow").css({opacity: 1});
-			$("#eye-left-bow").css({opacity: 1, bottom: "200px"});
+			
+			if(!$(this).hasClass("active")){					
+	 			$("#eye-left-shine").css({opacity: 1, "transform": "scale(0.8,0.8)"});
+				$("#eye-right-shine").css({opacity: 1, "transform": "scale(0.8,0.8)"});//	height: 45px; width: 46px;
+				$("#eye-right-bow").css({opacity: 1});
+				$("#eye-left-bow").css({opacity: 1, bottom: "200px"});	
+			} else {
+				$("#eye-right-bow").css({opacity: 1});
+				$("#eye-left-bow").css({opacity: 1, bottom: "200px"});
+			}
 		},
-		function(e){
+		function(e){	
  			$("#eye-left-shine").css({"transform": "scale(0.2,0.2)", opacity: 0});
 			$("#eye-right-shine").css({"transform": "scale(0.2,0.2)", opacity: 0});
 			$("#eye-left-bow").css({opacity: 0, bottom: "180px"});
 			$("#eye-right-bow").css({opacity: 0});
 		}
 	);
+		
+	/* content nav */
+	$(".navbar-nav li:not(.active) a, .navbar-brand").click(function(){
+		var id = $(this).data("id");
+		$(".content").hide();	
+		$(".navbar-nav li").removeClass("active");
+		
+		if(id){
+			$("#"+id).show();
+			$(this).parent().addClass("active");
+			// $(".bunny").css({"transform", ""})
+		}
+	});
+	
+	
 });
